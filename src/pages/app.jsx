6933c27905ff55212365048eec3d3b504dfc1d35
OskarMantilla21 <137963398+OskarMantilla21@users.noreply.github.com>
2023-11-14 14:33:22 -0500

    finished layout used grid

---
+++ src/pages/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../template/layout'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Layout>
+      </Layout>
+    </div>
+  );
+}
+export default App;
